007ee3f8af37392da9c5f1a78b0c0bc9826590fa
dacaiguoguo <dacaiguoguo@163.com>
2024-08-30 14:04:31 +0800

    Authorization successful

--- PySmartCrypto/rijndael.py
+++ PySmartCrypto/rijndael.py
@@ -1,55 +1,9 @@
-"""
-A pure python (slow) implementation of rijndael with a decent interface
-
-To include -
-
-from rijndael import rijndael
-
-To do a key setup -
-
-r = rijndael(key, block_size = 16)
-
-key must be a string of length 16, 24, or 32
-blocksize must be 16, 24, or 32. Default is 16
-
-To use -
-
-ciphertext = r.encrypt(plaintext)
-plaintext = r.decrypt(ciphertext)
-
-If any strings are of the wrong length a ValueError is thrown
-"""
-
-# ported from the Java reference code by Bram Cohen, bram@gawth.com, April 2001
-# this code is public domain, unless someone makes
-# an intellectual property claim against the reference
-# code, in which case it can be made public domain by
-# deleting all the comments and renaming all the variables
-
 import copy
-import string
-
-
-
-#-----------------------
-#TREV - ADDED BECAUSE THERE'S WARNINGS ABOUT INT OVERFLOW BEHAVIOR CHANGING IN
-#2.4.....
-import os, sys
-if os.name != "java" and sys.version_info < (3,0):
-    import exceptions
-    if hasattr(exceptions, "FutureWarning"):
-        import warnings
-        warnings.filterwarnings("ignore", category=FutureWarning, append=1)
-#-----------------------
-
-
 
 shifts = [[[0, 0], [1, 3], [2, 2], [3, 1]],
           [[0, 0], [1, 5], [2, 4], [3, 3]],
           [[0, 0], [1, 7], [3, 5], [4, 4]]]
 
-# [keysize][block_size]
-#num_rounds = {16: {16: 10, 24: 12, 32: 14}, 24: {16: 12, 24: 12, 32: 14}, 32: {16: 14, 24: 14, 32: 14}}
 num_rounds = {16: {16: 3}}
 
 A = [[1, 1, 1, 1, 1, 0, 0, 0],
@@ -61,8 +15,6 @@ A = [[1, 1, 1, 1, 1, 0, 0, 0],
      [1, 1, 1, 0, 0, 0, 1, 1],
      [1, 1, 1, 1, 0, 0, 0, 1]]
 
-# produce log and alog tables, needed for multiplying in the
-# field GF(2^m) (generator = 3)
 alog = [1]
 for i in range(255):
     j = (alog[-1] << 1) ^ alog[-1]
@@ -74,14 +26,12 @@ log = [0] * 256
 for i in range(1, 255):
     log[alog[i]] = i
 
-# multiply two elements of GF(2^m)
 def mul(a, b):
     if a == 0 or b == 0:
         return 0
     return alog[(log[a & 0xFF] + log[b & 0xFF]) % 255]
 
-# substitution box based on F^{-1}(x)
-box = [[0] * 8 for i in range(256)]
+box = [[0] * 8 for _ in range(256)]
 box[1][7] = 1
 for i in range(2, 256):
     j = alog[255 - log[i]]
@@ -90,35 +40,32 @@ for i in range(2, 256):
 
 B = [0, 1, 1, 0, 0, 0, 1, 1]
 
-# affine transform:  box[i] <- B + A*box[i]
-cox = [[0] * 8 for i in range(256)]
+cox = [[0] * 8 for _ in range(256)]
 for i in range(256):
     for t in range(8):
         cox[i][t] = B[t]
         for j in range(8):
             cox[i][t] ^= A[t][j] * box[i][j]
 
-# S-boxes and inverse S-boxes
-S =  [0] * 256
+S = [0] * 256
 Si = [0] * 256
 for i in range(256):
     S[i] = cox[i][0] << 7
     for t in range(1, 8):
-        S[i] ^= cox[i][t] << (7-t)
+        S[i] ^= cox[i][t] << (7 - t)
     Si[S[i] & 0xFF] = i
 
-# T-boxes
 G = [[2, 1, 1, 3],
-    [3, 2, 1, 1],
-    [1, 3, 2, 1],
-    [1, 1, 3, 2]]
+     [3, 2, 1, 1],
+     [1, 3, 2, 1],
+     [1, 1, 3, 2]]
 
-AA = [[0] * 8 for i in range(4)]
+AA = [[0] * 8 for _ in range(4)]
 
 for i in range(4):
     for j in range(4):
         AA[i][j] = G[i][j]
-        AA[i][i+4] = 1
+        AA[i][i + 4] = 1
 
 for i in range(4):
     pivot = AA[i][i]
@@ -126,20 +73,20 @@ for i in range(4):
         t = i + 1
         while AA[t][i] == 0 and t < 4:
             t += 1
-            assert t != 4, 'G matrix must be invertible'
-            for j in range(8):
-                AA[i][j], AA[t][j] = AA[t][j], AA[i][j]
-            pivot = AA[i][i]
+        assert t != 4, 'G matrix must be invertible'
+        for j in range(8):
+            AA[i][j], AA[t][j] = AA[t][j], AA[i][j]
+        pivot = AA[i][i]
     for j in range(8):
         if AA[i][j] != 0:
             AA[i][j] = alog[(255 + log[AA[i][j] & 0xFF] - log[pivot & 0xFF]) % 255]
     for t in range(4):
         if i != t:
-            for j in range(i+1, 8):
+            for j in range(i + 1, 8):
                 AA[t][j] ^= mul(AA[i][j], AA[t][i])
             AA[t][i] = 0
 
-iG = [[0] * 4 for i in range(4)]
+iG = [[0] * 4 for _ in range(4)]
 
 for i in range(4):
     for j in range(4):
@@ -186,7 +133,6 @@ for t in range(256):
     U3.append(mul4(t, iG[2]))
     U4.append(mul4(t, iG[3]))
 
-# round constants
 rcon = [1]
 r = 1
 for t in range(1, 30):
@@ -212,84 +158,78 @@ del cox
 del iG
 
 class rijndael:
-    def __init__(self, key, block_size = 16):
-        if block_size != 16 and block_size != 24 and block_size != 32:
-            raise ValueError('Invalid block size: ' + str(block_size))
-        if len(key) != 16 and len(key) != 24 and len(key) != 32:
-            raise ValueError('Invalid key size: ' + str(len(key)))
+    def __init__(self, key, block_size=16):
+        if block_size not in [16, 24, 32]:
+            raise ValueError(f'Invalid block size: {block_size}')
+        if len(key) not in [16, 24, 32]:
+            raise ValueError(f'Invalid key size: {len(key)}')
         self.block_size = block_size
 
         ROUNDS = num_rounds[len(key)][block_size]
-        BC = block_size / 4
-        # encryption round keys
-        Ke = [[0] * BC for i in range(ROUNDS + 1)]
-        # decryption round keys
-        Kd = [[0] * BC for i in range(ROUNDS + 1)]
+        BC = block_size // 4
+        Ke = [[0] * BC for _ in range(ROUNDS + 1)]
+        Kd = [[0] * BC for _ in range(ROUNDS + 1)]
         ROUND_KEY_COUNT = (ROUNDS + 1) * BC
-        KC = len(key) / 4
+        KC = len(key) // 4
 
-        # copy user material bytes into temporary ints
         tk = []
-        for i in range(0, KC):
-            tk.append((ord(key[i * 4]) << 24) | (ord(key[i * 4 + 1]) << 16) |
-                (ord(key[i * 4 + 2]) << 8) | ord(key[i * 4 + 3]))
+        for i in range(KC):
+            tk.append((key[i * 4] << 24) | (key[i * 4 + 1] << 16) |
+                      (key[i * 4 + 2] << 8) | key[i * 4 + 3])
 
-        # copy values into round key arrays
         t = 0
         j = 0
         while j < KC and t < ROUND_KEY_COUNT:
-            Ke[t / BC][t % BC] = tk[j]
-            Kd[ROUNDS - (t / BC)][t % BC] = tk[j]
+            Ke[t // BC][t % BC] = tk[j]
+            Kd[ROUNDS - (t // BC)][t % BC] = tk[j]
             j += 1
             t += 1
         tt = 0
         rconpointer = 0
         while t < ROUND_KEY_COUNT:
-            # extrapolate using phi (the round key evolution function)
             tt = tk[KC - 1]
-            tk[0] ^= (S[(tt >> 16) & 0xFF] & 0xFF) << 24 ^  \
-                     (S[(tt >>  8) & 0xFF] & 0xFF) << 16 ^  \
-                     (S[ tt        & 0xFF] & 0xFF) <<  8 ^  \
-                     (S[(tt >> 24) & 0xFF] & 0xFF)       ^  \
-                     (rcon[rconpointer]    & 0xFF) << 24
+            tk[0] ^= (S[(tt >> 16) & 0xFF] & 0xFF) << 24 ^ \
+                     (S[(tt >> 8) & 0xFF] & 0xFF) << 16 ^ \
+                     (S[tt & 0xFF] & 0xFF) << 8 ^ \
+                     (S[(tt >> 24) & 0xFF] & 0xFF) ^ \
+                     (rcon[rconpointer] & 0xFF) << 24
             rconpointer += 1
             if KC != 8:
                 for i in range(1, KC):
-                    tk[i] ^= tk[i-1]
+                    tk[i] ^= tk[i - 1]
             else:
-                for i in range(1, KC / 2):
-                    tk[i] ^= tk[i-1]
-                tt = tk[KC / 2 - 1]
-                tk[KC / 2] ^= (S[ tt        & 0xFF] & 0xFF)       ^ \
-                              (S[(tt >>  8) & 0xFF] & 0xFF) <<  8 ^ \
-                              (S[(tt >> 16) & 0xFF] & 0xFF) << 16 ^ \
-                              (S[(tt >> 24) & 0xFF] & 0xFF) << 24
-                for i in range(KC / 2 + 1, KC):
-                    tk[i] ^= tk[i-1]
-            # copy values into round key arrays
+                for i in range(1, KC // 2):
+                    tk[i] ^= tk[i - 1]
+                tt = tk[KC // 2 - 1]
+                tk[KC // 2] ^= (S[tt & 0xFF] & 0xFF) ^ \
+                               (S[(tt >> 8) & 0xFF] & 0xFF) << 8 ^ \
+                               (S[(tt >> 16) & 0xFF] & 0xFF) << 16 ^ \
+                               (S[(tt >> 24) & 0xFF] & 0xFF) << 24
+                for i in range(KC // 2 + 1, KC):
+                    tk[i] ^= tk[i - 1]
             j = 0
             while j < KC and t < ROUND_KEY_COUNT:
-                Ke[t / BC][t % BC] = tk[j]
-                Kd[ROUNDS - (t / BC)][t % BC] = tk[j]
+                Ke[t // BC][t % BC] = tk[j]
+                Kd[ROUNDS - (t // BC)][t % BC] = tk[j]
                 j += 1
                 t += 1
-        # inverse MixColumn where needed
+
         for r in range(1, ROUNDS):
             for j in range(BC):
                 tt = Kd[r][j]
                 Kd[r][j] = U1[(tt >> 24) & 0xFF] ^ \
                            U2[(tt >> 16) & 0xFF] ^ \
-                           U3[(tt >>  8) & 0xFF] ^ \
-                           U4[ tt        & 0xFF]
+                           U3[(tt >> 8) & 0xFF] ^ \
+                           U4[tt & 0xFF]
         self.Ke = Ke
         self.Kd = Kd
 
     def encrypt(self, plaintext):
         if len(plaintext) != self.block_size:
-            raise ValueError('wrong block length, expected ' + str(self.block_size) + ' got ' + str(len(plaintext)))
+            raise ValueError(f'wrong block length, expected {self.block_size} got {len(plaintext)}')
         Ke = self.Ke
 
-        BC = self.block_size / 4
+        BC = self.block_size // 4
         ROUNDS = len(Ke) - 1
         if BC == 4:
             SC = 0
@@ -301,38 +241,34 @@ class rijndael:
         s2 = shifts[SC][2][0]
         s3 = shifts[SC][3][0]
         a = [0] * BC
-        # temporary work array
         t = []
-        # plaintext to ints + key
         for i in range(BC):
-            t.append((ord(plaintext[i * 4    ]) << 24 |
-                      ord(plaintext[i * 4 + 1]) << 16 |
-                      ord(plaintext[i * 4 + 2]) <<  8 |
-                      ord(plaintext[i * 4 + 3])        ) ^ Ke[0][i])
-        # apply round transforms
+            t.append((plaintext[i * 4] << 24 |
+                      plaintext[i * 4 + 1] << 16 |
+                      plaintext[i * 4 + 2] << 8 |
+                      plaintext[i * 4 + 3]) ^ Ke[0][i])
         for r in range(1, ROUNDS):
             for i in range(BC):
-                a[i] = (T1[(t[ i           ] >> 24) & 0xFF] ^
+                a[i] = (T1[(t[i] >> 24) & 0xFF] ^
                         T2[(t[(i + s1) % BC] >> 16) & 0xFF] ^
-                        T3[(t[(i + s2) % BC] >>  8) & 0xFF] ^
-                        T4[ t[(i + s3) % BC]        & 0xFF]  ) ^ Ke[r][i]
+                        T3[(t[(i + s2) % BC] >> 8) & 0xFF] ^
+                        T4[t[(i + s3) % BC] & 0xFF]) ^ Ke[r][i]
             t = copy.copy(a)
-        # last round is special
         result = []
         for i in range(BC):
             tt = Ke[ROUNDS][i]
-            result.append((S[(t[ i           ] >> 24) & 0xFF] ^ (tt >> 24)) & 0xFF)
+            result.append((S[(t[i] >> 24) & 0xFF] ^ (tt >> 24)) & 0xFF)
             result.append((S[(t[(i + s1) % BC] >> 16) & 0xFF] ^ (tt >> 16)) & 0xFF)
-            result.append((S[(t[(i + s2) % BC] >>  8) & 0xFF] ^ (tt >>  8)) & 0xFF)
-            result.append((S[ t[(i + s3) % BC]        & 0xFF] ^  tt       ) & 0xFF)
-        return string.join(map(chr, result), '')
+            result.append((S[(t[(i + s2) % BC] >> 8) & 0xFF] ^ (tt >> 8)) & 0xFF)
+            result.append((S[t[(i + s3) % BC] & 0xFF] ^ tt) & 0xFF)
+        return bytes(result)
 
     def decrypt(self, ciphertext):
         if len(ciphertext) != self.block_size:
-            raise ValueError('wrong block length, expected ' + str(self.block_size) + ' got ' + str(len(plaintext)))
+            raise ValueError(f'wrong block length, expected {self.block_size} got {len(ciphertext)}')
         Kd = self.Kd
 
-        BC = self.block_size / 4
+        BC = self.block_size // 4
         ROUNDS = len(Kd) - 1
         if BC == 4:
             SC = 0
@@ -344,31 +280,27 @@ class rijndael:
         s2 = shifts[SC][2][1]
         s3 = shifts[SC][3][1]
         a = [0] * BC
-        # temporary work array
         t = [0] * BC
-        # ciphertext to ints + key
         for i in range(BC):
-            t[i] = (ord(ciphertext[i * 4    ]) << 24 |
-                    ord(ciphertext[i * 4 + 1]) << 16 |
-                    ord(ciphertext[i * 4 + 2]) <<  8 |
-                    ord(ciphertext[i * 4 + 3])        ) ^ Kd[0][i]
-        # apply round transforms
+            t[i] = (ciphertext[i * 4] << 24 |
+                    ciphertext[i * 4 + 1] << 16 |
+                    ciphertext[i * 4 + 2] << 8 |
+                    ciphertext[i * 4 + 3]) ^ Kd[0][i]
         for r in range(1, ROUNDS):
             for i in range(BC):
-                a[i] = (T5[(t[ i           ] >> 24) & 0xFF] ^
+                a[i] = (T5[(t[i] >> 24) & 0xFF] ^
                         T6[(t[(i + s1) % BC] >> 16) & 0xFF] ^
-                        T7[(t[(i + s2) % BC] >>  8) & 0xFF] ^
-                        T8[ t[(i + s3) % BC]        & 0xFF]  ) ^ Kd[r][i]
+                        T7[(t[(i + s2) % BC] >> 8) & 0xFF] ^
+                        T8[t[(i + s3) % BC] & 0xFF]) ^ Kd[r][i]
             t = copy.copy(a)
-        # last round is special
         result = []
         for i in range(BC):
             tt = Kd[ROUNDS][i]
-            result.append((Si[(t[ i           ] >> 24) & 0xFF] ^ (tt >> 24)) & 0xFF)
+            result.append((Si[(t[i] >> 24) & 0xFF] ^ (tt >> 24)) & 0xFF)
             result.append((Si[(t[(i + s1) % BC] >> 16) & 0xFF] ^ (tt >> 16)) & 0xFF)
-            result.append((Si[(t[(i + s2) % BC] >>  8) & 0xFF] ^ (tt >>  8)) & 0xFF)
-            result.append((Si[ t[(i + s3) % BC]        & 0xFF] ^  tt       ) & 0xFF)
-        return string.join(map(chr, result), '')
+            result.append((Si[(t[(i + s2) % BC] >> 8) & 0xFF] ^ (tt >> 8)) & 0xFF)
+            result.append((Si[t[(i + s3) % BC] & 0xFF] ^ tt) & 0xFF)
+        return bytes(result)
 
 def encrypt(key, block):
     return rijndael(key, len(block)).encrypt(block)
@@ -378,8 +310,8 @@ def decrypt(key, block):
 
 def test():
     def t(kl, bl):
-        b = 'b' * bl
-        r = rijndael('a' * kl, bl)
+        b = b'b' * bl
+        r = rijndael(b'a' * kl, bl)
         assert r.decrypt(r.encrypt(b)) == b
     t(16, 16)
     t(16, 24)
@@ -390,3 +322,6 @@ def test():
     t(32, 16)
     t(32, 24)
     t(32, 32)
+
+if __name__ == "__main__":
+    test()
--- PySmartCrypto/smartcrypto.py
+++ PySmartCrypto/smartcrypto.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 import crypto
 import sys
 import re
@@ -6,16 +5,17 @@ import requests
 
 UserId = "654321"
 AppId = "12345"
-deviceId =  "7e509404-9d7c-46b4-8f6a-e2a9668ad184"
-tvIP = "192.168.0.21"
+deviceId = "7e509404-9d7c-46b4-8f6a-e2a9668ad184"
+tvIP = "192.168.50.254"
 tvPort = "8080"
 lastRequestId = 0
+
 def getFullUrl(urlPath):
     global tvIP, tvPort
-    return "http://"+tvIP+":"+tvPort+urlPath
+    return f"http://{tvIP}:{tvPort}{urlPath}"
 
 def GetFullRequestUri(step, appId, deviceId):
-    return getFullUrl("/ws/pairing?step="+str(step)+"&app_id="+appId+"&device_id="+deviceId)
+    return getFullUrl(f"/ws/pairing?step={step}&app_id={appId}&device_id={deviceId}")
 
 def ShowPinPageOnTv():
     requests.post(getFullUrl("/ws/apps/CloudPINPage"), "pin4")
@@ -26,33 +26,34 @@ def CheckPinPageOnTv():
     output = re.search('state>([^<>]*)</state>', page, flags=re.IGNORECASE)
     if output is not None:
         state = output.group(1)
-        print("Current state: "+state)
+        print(f"Current state: {state}")
         if state == "stopped":
             return True
     return False
 
 def FirstStepOfPairing():
-    global AppId, deviceId;
-    firstStepURL = GetFullRequestUri(0,AppId, deviceId)+"&type=1"
+    global AppId, deviceId
+    firstStepURL = f"{GetFullRequestUri(0, AppId, deviceId)}&type=1"
     firstStepResponse = requests.get(firstStepURL).text
 
 def StartPairing():
     global lastRequestId
-    lastRequestId=0
+    lastRequestId = 0
     if CheckPinPageOnTv():
         print("Pin NOT on TV")
         ShowPinPageOnTv()
     else:
-        print("Pin ON TV");
+        print("Pin ON TV")
+
 def HelloExchange(pin):
     global AppId, deviceId, lastRequestId, UserId
-    hello_output = crypto.generateServerHello(UserId,pin)
+    hello_output = crypto.generateServerHello(UserId, pin)
     if not hello_output:
         return False
-    content = "{\"auth_Data\":{\"auth_type\":\"SPC\",\"GeneratorServerHello\":\"" + hello_output['serverHello'].encode('hex').upper() + "\"}}"
+    content = {"auth_Data": {"auth_type": "SPC", "GeneratorServerHello": hello_output['serverHello'].hex().upper()}}
     secondStepURL = GetFullRequestUri(1, AppId, deviceId)
-    secondStepResponse = requests.post(secondStepURL, content).text
-    print('secondStepResponse: ' + secondStepResponse)
+    secondStepResponse = requests.post(secondStepURL, json=content).text
+    print(f'secondStepResponse: {secondStepResponse}')
     output = re.search('request_id.*?(\d).*?GeneratorClientHello.*?:.*?(\d[0-9a-zA-Z]*)', secondStepResponse, flags=re.IGNORECASE)
     if output is None:
         return False
@@ -62,27 +63,28 @@ def HelloExchange(pin):
     return crypto.parseClientHello(clientHello, hello_output['hash'], hello_output['AES_key'], UserId)
 
 def AcknowledgeExchange(SKPrime):
-    global lastRequestId, AppId,  deviceId;
+    global lastRequestId, AppId, deviceId
     serverAckMessage = crypto.generateServerAcknowledge(SKPrime)
-    content="{\"auth_Data\":{\"auth_type\":\"SPC\",\"request_id\":\"" + str(lastRequestId) + "\",\"ServerAckMsg\":\"" + serverAckMessage + "\"}}"
+    content = {"auth_Data": {"auth_type": "SPC", "request_id": str(lastRequestId), "ServerAckMsg": serverAckMessage}}
     thirdStepURL = GetFullRequestUri(2, AppId, deviceId)
-    thirdStepResponse = requests.post(thirdStepURL, content).text
+    thirdStepResponse = requests.post(thirdStepURL, json=content).text
     if "secure-mode" in thirdStepResponse:
         print("TODO: Implement handling of encryption flag!!!!")
         sys.exit(-1)
     output = re.search('ClientAckMsg.*?:.*?(\d[0-9a-zA-Z]*).*?session_id.*?(\d)', thirdStepResponse, flags=re.IGNORECASE)
     if output is None:
-        print("Unable to get session_id and/or ClientAckMsg!!!");
+        print("Unable to get session_id and/or ClientAckMsg!!!")
         sys.exit(-1)
     clientAck = output.group(1)
     if not crypto.parseClientAcknowledge(clientAck, SKPrime):
         print("Parse client ac message failed.")
         sys.exit(-1)
-    sessionId=output.group(2)
-    print("sessionId: "+sessionId)
+    sessionId = output.group(2)
+    print(f"sessionId: {sessionId}")
     return sessionId
+
 def ClosePinPageOnTv():
-    full_url = getFullUrl("/ws/apps/CloudPINPage/run");
+    full_url = getFullUrl("/ws/apps/CloudPINPage/run")
     requests.delete(full_url)
     return False
 
@@ -90,19 +92,18 @@ StartPairing()
 ctx = False
 SKPrime = False
 while not ctx:
-    tvPIN = raw_input("Please enter pin from tv: ")
-    print("Got pin: '"+tvPIN+"'\n")
+    tvPIN = input("Please enter pin from tv: ")
+    print(f"Got pin: '{tvPIN}'\n")
     FirstStepOfPairing()
     output = HelloExchange(tvPIN)
     if output:
         ctx = output['ctx']
         SKPrime = output['SKPrime']
-        print("ctx: " + ctx.encode('hex'))
+        print(f"ctx: {ctx.hex()}")
         print("Pin accepted :)\n")
     else:
         print("Pin incorrect. Please try again...\n")
 
 currentSessionId = AcknowledgeExchange(SKPrime)
 ClosePinPageOnTv()
-print("Authorization successfull :)\n")
-
+print("Authorization successful :)\n")
